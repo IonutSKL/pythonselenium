fe974d36518d2fc1d7562e6db8a21aadb380b76e
IonutSKL <scarlatalinn@gmail.com>
2019-11-25 22:28:49 +0100

    test

--- Ex_Files_Python_Automation_Testing/Exercise Files/CH01/01_02/code_01_02.py
+++ Ex_Files_Python_Automation_Testing/Exercise Files/CH01/01_02/code_01_02.py
@@ -1,4 +1,11 @@
-from selenium import webdriver;
-browser= webdriver.Firefox();
-browser.get('http://www.seleniumhq.org');
+import time
+from selenium import webdriver
 
+driver = webdriver.Chrome('D:\chromedriver')  # Optional argument, if not specified will search path.
+driver.get('http://www.google.com/');
+time.sleep(5) # Let the user actually see something!
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+time.sleep(5) # Let the user actually see something!
+driver.quit()
