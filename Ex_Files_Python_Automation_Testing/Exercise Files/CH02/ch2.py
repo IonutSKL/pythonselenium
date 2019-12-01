2ae26debfd0083c83636211b8ca2b7eb570f85bf
IonutSKL <scarlatalinn@gmail.com>
2019-12-01 19:33:56 +0100

    work code

---
+++ Ex_Files_Python_Automation_Testing/Exercise Files/CH02/ch2.py
@@ -0,0 +1,26 @@
+import time
+from selenium import webdriver
+
+driver = webdriver.Chrome('D:\chromedriver')  # Optional argument, if not specified will search path.
+driver.get('file:///D:/LinkedIn%20Trainings/Python%20Testing%20Automation/Ex_Files_Python_Automation_Testing/Exercise%20Files/CH02/html_code_02.html')
+
+login_form = driver.find_element_by_id('loginForm')
+print('login form is: ', login_form)
+
+username = driver.find_element_by_name('username')
+print('username is: ', username)
+
+login_form_absolute = driver.find_element_by_xpath('/html/body/form[1]/input')
+login_form_relative = driver.find_element_by_xpath('//form[1]/input')
+login_form_id = driver.find_element_by_xpath("//input[@name='username']")
+username.send_keys('scarlat')
+
+content = driver.find_element_by_class_name('content')
+
+print("content", content)
+print("abs:", login_form_absolute)
+print("relative:", login_form_relative)
+print("id:", login_form_id)
+
+time.sleep(5)
+driver.close()
---
+++ Ex_Files_Python_Automation_Testing/Exercise Files/CH02/challenge.py
@@ -0,0 +1,27 @@
+import time
+from selenium import webdriver
+
+driver = webdriver.Chrome('D:\chromedriver')  # Optional argument, if not specified will search path.
+driver.get('https://selenium.dev/')
+
+search_box = driver.find_element_by_xpath("//input[@id='gsc-i-id1']")
+search_box.send_keys('Selenium')
+print("search_box element: ", search_box)
+
+#element_id = driver.find_element_by_id('q')
+#print(element_id)
+
+#element_name = driver.find_element_by_name('q')
+#print(element_name)
+
+heading = driver.find_element_by_xpath("//section[@class='hero homepage']/h1")
+print('header', heading)
+
+sub_header = driver.find_element_by_xpath("//h1[@class='sub-header']")
+print('sub-header', sub_header)
+
+#element_classname = driver.find_element_by_class_name('selenium-sponsors')
+#print('classname', element_classname)
+
+time.sleep(5)
+driver.close()
